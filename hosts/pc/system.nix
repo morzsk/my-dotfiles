7bc8a2e99aa4a174d8c21d62bbad59a384d210ae
mo <morzsk@pm.me>
2026-05-08 19:16:55 +0100

    Added modules to pc host

---
+++ hosts/pc/system.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+
+let
+  modules = ../../modules/system;
+in
+{
+  networking.hostName = "pc";
+  programs.zsh.enable = true;
+
+  users.users.mo = {
+    isNormalUser = true;
+    description = "mo";
+    home = "/home/mo";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+  };
+
+  imports = [
+    (modules + /system)
+    (modules + /nvidia)
+    (modules + /networking)
+    (modules + /tailscale)
+    (modules + /pipewire)
+    (modules + /nix)
+    (modules + /stylix)
+    (modules + /wine)
+    (modules + /ly)
+  ];
+}
---
+++ hosts/pc/user.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+let
+  modules = ../../modules/user;
+in
+{
+  programs.home-manager.enable = true;
+
+  home = {
+    username = "mo";
+    homeDirectory = "/home/mo";
+
+    stateVersion = "24.05";
+
+    file = { };
+    sessionVariables = { };
+  };
+
+  imports = [
+    (modules + /git)
+    (modules + /zsh)
+    (modules + /utils)
+    (modules + /hyprland)
+    (modules + /stylix)
+    (modules + /gtk)
+    (modules + /rofi)
+    (modules + /yazi)
+    (modules + /kitty)
+    (modules + /flatpak)
+    (modules + /apps)
+    (modules + /brave)
+    (modules + /obsidian)
+    (modules + /vscode)
+  ];
+}
