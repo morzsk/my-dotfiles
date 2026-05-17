31e5435a3f9d49c85870fae684b087c254ba7812
mo <morzsk@pm.me>
2026-05-17 20:08:17 +0000

    Fixed Opencode default, added opencode to user modules

--- modules/user/opencode/default.nix
+++ modules/user/opencode/default.nix
@@ -1,24 +1,38 @@
 { config, pkgs, ... }:
 
 {
-    programs.opencode = {
-        enable = true;
-        settings = {
-			"$schema" = "https://opencode.ai/config.json";
-			provider = {
-				lmstudio = {
-					models = {
-						"qwen/qwen3.5-35b-a3b" = {
-							name = "QWEN 3.5 (local)";
-						};
-					};
-					name = "LM Studio (local)";
-					npm = "@ai-sdk/openai-compatible";
-					options = {
-						baseURL = "http://10.8.0.6:1234/v1";
-					};
-				};
-			};
+  programs.opencode = {
+    enable = true;
+    settings = {
+      "$schema" = "https://opencode.ai/config.json";
+      model = "mistralai/devstral-small-2-2512";
+      provider = {
+        lmstudio = {
+          name = "LM Studio (local)";
+          npm = "@ai-sdk/openai-compatible";
+          options = {
+            baseURL = "http://127.0.0.1:1234/v1";
+          };
+          models = {
+            "mistralai/devstral-small-2-2512" = {
+              name = "Mistralai/Devstral Small 2 2512 (local)";
+              modalities = {
+                input = [ "text" "image" ];
+                output = [ "text" ];
+              };
+              limit = {
+                context = 65536;
+                output = 4096;
+              };
+            };
+          };
         };
+      };
+      lsp = {
+        typescript = {
+          disabled = true;
+        };
+      };
     };
+  };
 }
