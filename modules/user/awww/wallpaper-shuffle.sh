6b1db040c6adc64b99997d8f35c599acfb6fc034
mo <morzsk@pm.me>
2026-05-30 09:55:31 +0000

    Fixed awww being asleep

--- modules/user/awww/wallpaper-shuffle.sh
+++ modules/user/awww/wallpaper-shuffle.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+until systemctl --user is-active --quiet awww.service; do
+  sleep 1
+done
+
 while true; do
   find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" \) \
     | shuf \
