72fe431b7940faab2f030bc009d132ead9efb0ad
mo <morzsk@pm.me>
2026-05-31 21:41:47 +0000

    Added some brave policies

--- modules/system/brave/default.nix
+++ modules/system/brave/default.nix
@@ -1,6 +1,14 @@
 { ... }:
 
 {
+  environment.etc."brave/policies/managed/settings.json".text = builtins.toJSON {
+    BraveWalletDisabled = true;
+    BraveRewardDisabled = true;
+    BraveVPNDisabled = true;
+    BraveNewsDisabled = true;
+    BraveTalkDisabled = true;
+  };
+
   environment.etc."brave/policies/managed/extensions.json".text = builtins.toJSON {
     ExtensionInstallForcelist = [
       "ghmbeldphafepmbegfdlkpapadhbakde;https://clients2.google.com/service/update2/crx"
