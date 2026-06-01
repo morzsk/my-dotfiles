15c0a7b6fbe91ea5fb656a46b87a5fa9ce2ffc5c
mo <morzsk@pm.me>
2026-06-01 18:56:00 +0000

    Added glyphs for lsps

--- modules/user/nvchad/extra.lua
+++ modules/user/nvchad/extra.lua
@@ -1,1 +1,12 @@
 vim.lsp.enable({ "nil_ls", "ts_ls", "omnisharp", "zls", "clangd", "bashls" })
+
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "󰅚",
+      [vim.diagnostic.severity.WARN]  = "󰀪",
+      [vim.diagnostic.severity.INFO]  = "󰋽",
+      [vim.diagnostic.severity.HINT]  = "󰌵",
+    },
+  },
+})
