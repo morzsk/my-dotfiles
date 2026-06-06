a97c4083b14d19111635024d1cf9c078053b690c
mo <morzsk@pm.me>
2026-06-06 10:12:18 +0000

    Record main mic input

--- modules/user/gpu-screen-recorder/default.nix
+++ modules/user/gpu-screen-recorder/default.nix
@@ -5,24 +5,32 @@ let
 
   clipSave = pkgs.writeShellApplication {
     name = "clip-save";
-    runtimeInputs = with pkgs; [ ffmpeg libnotify bc procps ];
+    runtimeInputs = with pkgs; [
+      ffmpeg
+      libnotify
+      bc
+      procps
+    ];
     text = builtins.readFile ./clip-save.sh;
   };
 in
 {
-  home.packages = [ clipSave pkgs.gpu-screen-recorder ];
+  home.packages = [
+    clipSave
+    pkgs.gpu-screen-recorder
+  ];
 
   home.sessionVariables.XDG_CLIPS_DIR = clipsDir;
 
   systemd.user.services.gpu-screen-recorder = {
     Unit = {
       Description = "GPU Screen Recorder replay buffer";
-      After       = [ "graphical-session.target" ];
-      PartOf      = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
+      PartOf = [ "graphical-session.target" ];
     };
     Service = {
-      ExecStart = "${pkgs.gpu-screen-recorder}/bin/gpu-screen-recorder -w HDMI-A-1 -f 60 -a default_output -c mp4 -r 600 -o ${clipsDir}";
-      Restart    = "on-failure";
+      ExecStart = "${pkgs.gpu-screen-recorder}/bin/gpu-screen-recorder -w HDMI-A-1 -f 60 -a default_output -a default_input -c mp4 -r 600 -o ${clipsDir}";
+      Restart = "on-failure";
       RestartSec = "5";
     };
     Install.WantedBy = [ "graphical-session.target" ];
